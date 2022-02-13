b9df3795d54875377c237b9267429472026eb843
Eren Demir <78754731+Erend0@users.noreply.github.com>
2022-02-13 14:35:20 +0000

    Version 1.0
    Added Classes
    - Line
    -Rectangle
    -Ellipse
    -Circle

---
+++ OOPDraw2/Class5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace OOPDraw2
+{
+  
+        public class Circle : Shape
+        {
+            public Circle(Pen p, int x1, int y1) : base(p, x1, y1)
+            {
+            }
+            public Circle(Pen p, int x1, int y1, int x2, int y2) : base(p, x1, y1, x2, y2)
+            {
+                GrowTo(x2, y2);
+            }
+            public override void Draw(Graphics g)
+            {
+            DrawingFunctions.DrawClosedArc(g, this);
+        }
+            public virtual void GrowTo(int x2, int y2)
+            {
+                int diameter = Math.Max(x2 - X1, y2 - Y1);
+                X2 = X1 + diameter;
+                Y2 = Y1 + diameter;
+            }
+        }
+    
+}
---
+++ OOPDraw2/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace OOPDraw2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.LineWidth = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Colour = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Shape = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Action = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.Canvas.Location = new System.Drawing.Point(1, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(660, 438);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+            // 
+            // LineWidth
+            // 
+            this.LineWidth.FormattingEnabled = true;
+            this.LineWidth.Items.AddRange(new object[] {
+            "Thin ",
+            "Medium",
+            "Thick"});
+            this.LineWidth.Location = new System.Drawing.Point(667, 27);
+            this.LineWidth.Name = "LineWidth";
+            this.LineWidth.Size = new System.Drawing.Size(121, 21);
+            this.LineWidth.TabIndex = 1;
+            this.LineWidth.SelectedIndexChanged += new System.EventHandler(this.LineWidth_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(664, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Line Width";
+            // 
+            // Colour
+            // 
+            this.Colour.FormattingEnabled = true;
+            this.Colour.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue "});
+            this.Colour.Location = new System.Drawing.Point(667, 85);
+            this.Colour.Name = "Colour";
+            this.Colour.Size = new System.Drawing.Size(121, 21);
+            this.Colour.TabIndex = 3;
+            this.Colour.SelectedIndexChanged += new System.EventHandler(this.Colour_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(664, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Colour";
+            // 
+            // Shape
+            // 
+            this.Shape.FormattingEnabled = true;
+            this.Shape.Items.AddRange(new object[] {
+            "Line",
+            "Rectangle",
+            "Ellipse",
+            "Circle"});
+            this.Shape.Location = new System.Drawing.Point(667, 149);
+            this.Shape.Name = "Shape";
+            this.Shape.Size = new System.Drawing.Size(121, 21);
+            this.Shape.TabIndex = 5;
+            this.Shape.SelectedIndexChanged += new System.EventHandler(this.Shape_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(663, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Shape";
+            // 
+            // Action
+            // 
+            this.Action.FormattingEnabled = true;
+            this.Action.Items.AddRange(new object[] {
+            "Draw",
+            "Move",
+            "Select"});
+            this.Action.Location = new System.Drawing.Point(667, 213);
+            this.Action.Name = "Action";
+            this.Action.Size = new System.Drawing.Size(121, 21);
+            this.Action.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(664, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Action";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 446);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Action);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Shape);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Colour);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LineWidth);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "OOPDraw";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.ComboBox LineWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Colour;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Shape;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox Action;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
